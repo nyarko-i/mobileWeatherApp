fad4c77c2b88dbaea3c5a829a206354d25d3fa74
nyarko-i <inyarko98@gmail.com>
2024-02-14 08:28:33 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { View, Text, SafeAreaView, StyleSheet } from "react-native"
+import { Fontisto } from '@expo/vector-icons'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Fontisto name="day-sunny" size={100} color="black" />
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>feelslike 5</Text>
+        <View style={styles.highLowWrapper}>
+        <Text style={styles.highLow}>High: 8</Text>
+        <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>Its sunny</Text>
+        <Text style={styles.message}>It's perfect t-shirt weather</Text>
+      </View>
+        
+
+      
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  wrapper: {
+    backgroundColor: "pink",
+    flex: 1
   },
-});
+  container:{
+    flex: 1,
+    alignItems: 'center'
+
+  },
+  temp:{
+    color: "black",
+    fontSize: 48
+  },
+  feels:{
+    color: "black",
+    fontSize: 30
+  },
+  highLow:{
+    color: "black",
+    fontSize: 20
+  },
+  highLowWrapper:{
+    flexDirection: 'row'
+  },
+  bodyWrapper:{
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  description:{
+    fontSize: 48
+  },
+
+  message:{
+    fontSize: 30,  }
+
+
+})
+
+export default App
